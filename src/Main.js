ac56afa71b8c305cd61d4dc6a35e6eb826e0396b
brambles <qjnight@gmail.com>
2015-10-16 02:35:13 +0800

    搞定

--- src/Main.js
+++ src/Main.js
@@ -7,7 +7,7 @@ define(['./Class', './Lexer', './Parser'], function(Class, Lexer, Parser){
       var lexer = Lexer();
       var parser = Parser();
 
-      var token_stream = lexer.parse(source_code);
+      var token_stream = lexer.parse(source_code+'\n');
       var ast = parser.parse(token_stream);
       var target_code = ast.toJavaScript();
 
