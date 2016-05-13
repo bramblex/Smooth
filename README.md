f93c4ad3aa93adee31255fe908f70e068813ddc4
brambles <qjnight@gmail.com>
2016-05-14 00:48:33 +0800

    Haskell is better than PureScript

--- README.md
+++
@@ -1,5 +0,0 @@
-# Smooth
-
-**原项目 BlxScript 更名为 Smooth。**
-
-正在用 PureScript 重构中
--- test/Main.purs
+++
@@ -1,9 +0,0 @@
-module Test.Main where
-
-import Prelude
-import Control.Monad.Eff
-import Control.Monad.Eff.Console
-
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "You should add some tests."
